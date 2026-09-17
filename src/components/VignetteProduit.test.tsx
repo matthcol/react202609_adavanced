c62d589aa95a30f30d23d918c55f6dc77754ef6d
matthcol <matthias.colin@gmail.com>
2026-09-17 11:52:54 +0200

    setup 1st test

--- src/components/VignetteProduit.test.tsx
+++ src/components/VignetteProduit.test.tsx
@@ -1,7 +1,39 @@
-import { describe, it } from 'vitest'
+import { render } from '@testing-library/react'
+import { describe, it, vi } from 'vitest'
+import type { Produit } from '../types/produit'
+import VignetteProduit from './VignetteProduit'
+import type { Panier } from '../types/panier'
+import { ContextPanier } from '../contextes/ContextPanier'
+
+// setup
+const produit: Produit = {
+    Id:92,
+    Libelle: "Chips Pom'lisse Nature - 6x30g",
+    Prix:1.47,
+    PhotoListe: "https:\/\/fd4-photos.leclercdrive.fr\/image.ashx?id=2917317&use=d&cat=p&typeid=i",
+    PhotoDetail: "https:\/\/fd4-photos.leclercdrive.fr\/image.ashx?id=2917316&use=l&cat=p&typeid=i",
+    FiltresMarque: ["Marque Rep\u00e8re","Pom' lisse"],
+    FiltresEnCeMoment:[],
+    FiltresLabelsQualite:[],
+    FiltresNutriscore:["B"],
+    IdRangement: "284319|284424|284447",
+    Url: "https:\/\/fd4-courses.leclercdrive.fr\/magasin-103101-Roques-sur-Garonne-Toulouse\/fiche-produits-92--.aspx"
+}
+
+function renderVignette() {
+    const panier: Panier = []; // TODO: à gérer en param
+    render(
+        <ContextPanier.Provider value={{ panier, dispatch: vi.fn() }}>
+            <VignetteProduit produit={produit} />
+        </ContextPanier.Provider>
+    )
+}
 
 describe('VignetteProduit', () => {
-    it('dummy test', () => {
-        // test something
+
+    it('display produit with all infos', () => {
+        // render Vignette
+        renderVignette();
+        // verify display
     })
 })
