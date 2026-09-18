eb77bc2799a4e70b508083f43a5e5b2b2eae2c40
matthcol <matthias.colin@gmail.com>
2026-09-18 11:36:04 +0200

    update dependencies

--- src/components/VignetteProduit.test.tsx
+++ src/components/VignetteProduit.test.tsx
@@ -10,14 +10,14 @@ const produit: Produit = {
     Id:92,
     Libelle: "Chips Pom'lisse Nature - 6x30g",
     Prix:1.47,
-    PhotoListe: "https:\/\/fd4-photos.leclercdrive.fr\/image.ashx?id=2917317&use=d&cat=p&typeid=i",
-    PhotoDetail: "https:\/\/fd4-photos.leclercdrive.fr\/image.ashx?id=2917316&use=l&cat=p&typeid=i",
+    PhotoListe: "https://fd4-photos.leclercdrive.fr/image.ashx?id=2917317&use=d&cat=p&typeid=i",
+    PhotoDetail: "https://fd4-photos.leclercdrive.fr/image.ashx?id=2917316&use=l&cat=p&typeid=i",
     FiltresMarque: ["Marque Rep\u00e8re","Pom' lisse"],
     FiltresEnCeMoment:[],
     FiltresLabelsQualite:[],
     FiltresNutriscore:["B"],
     IdRangement: "284319|284424|284447",
-    Url: "https:\/\/fd4-courses.leclercdrive.fr\/magasin-103101-Roques-sur-Garonne-Toulouse\/fiche-produits-92--.aspx"
+    Url: "https://fd4-courses.leclercdrive.fr/magasin-103101-Roques-sur-Garonne-Toulouse/fiche-produits-92--.aspx"
 }
 
 function renderVignette() {
